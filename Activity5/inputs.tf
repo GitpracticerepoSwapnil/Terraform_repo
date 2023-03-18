1332cdd60ffe1aa5023f6b770393e208bd44cdf4
qtdevops <qtdevops@gmail.com>
2023-03-18 18:28:23 +0530

    Formatted

--- Activity5/inputs.tf
+++ Activity5/inputs.tf
@@ -1,18 +1,18 @@
 variable "region" {
-type = string
+  type = string
 
 }
 
 variable "vpcd" {
   type = object({
-   cidr_block = list(string)
-   Name = list(string)
+    cidr_block = list(string)
+    Name       = list(string)
   })
-  
+
 }
 
 variable "Subnet" {
-    type = object({
-        Name = list(string)
-    })
+  type = object({
+    Name = list(string)
+  })
 }
